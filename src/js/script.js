9de8de2eb5366e8b592d9ffa37ac18cd0e9edcbe
takanashi66 <t.inoue@funaffect.jp>
2018-09-30 22:33:43 +0900

    計算を実装

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,58 @@
 import Vue from 'vue'
 
-new Vue({
-  template: '<p>{{msg}}</p>',
-  data: {msg: 'hello world?'}
-}).$mount('#app')
+const item = [
+  {
+    name: '鉛筆',
+    price: 300,
+    quantity: 0
+  },
+  {
+    name: 'ノート',
+    price: 400,
+    quantity: 0
+  },
+  {
+    name: '消しゴム',
+    price: 500,
+    quantity: 0
+  },
+]
+
+const vm = new Vue({
+  el: '#app',
+  template: 
+  '<div>'+
+    '<ul>'+
+      '<li v-for="item in items">{{item.name}}: {{item.price}} x <input type="number" v-on:change="item.quantity = $event.target.value" v-bind:value="item.quantity" min="0" /> = {{item.price * item.quantity}}</li>'+
+    '</ul>'+
+    '<p :class="{error: !canBuy}">税込: {{totalPriceWithTax | numberWithDelimiter}}円</p>'+
+    '<p v-if="!canBuy">{{1000 | numberWithDelimiter}}円からご購入いただけます。</p>'+
+    '<p v-else>ご購入いただけます。</p>'+
+  '</div>',
+  data: {
+    items: item
+  },
+  
+  filters: {
+    numberWithDelimiter: function(value){
+      if(!value){
+        return '0';
+      }
+      return value.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1,')
+    }
+  },
+  
+  computed:{
+    totalPrice: function(){
+      return this.items.reduce(function(sum, item){
+        return sum + (item.price * item.quantity)
+      }, 0)
+    },
+    totalPriceWithTax: function(){
+      return Math.floor(this.totalPrice * 1.08)
+    },
+    canBuy: function(){
+      return this.totalPrice >= 1000
+    }
+  }
+})
